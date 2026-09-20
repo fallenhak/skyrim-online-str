5b97bf87641b98cfd35e4b5e5ef9e2b1559ef2a2
fallenhak <135631505+fallenhak@users.noreply.github.com>
2026-09-20 15:36:41 +0300

    refactor: rename world authority availability state

--- Code/client/Events/AuthorityChangedEvent.h
+++ Code/client/Events/AuthorityChangedEvent.h
@@ -6,6 +6,6 @@ struct AuthorityChangedEvent
 {
     bool HasLocalActorAuthority{false};
     bool HasLocalWorldAuthority{false};
-    bool HasWorldAuthorityGroup{false};
+    bool HasWorldAuthoritySource{false};
     uint32_t WorldAuthorityPlayerId{0};
 };
