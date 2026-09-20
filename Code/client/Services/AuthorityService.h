9755a07646119817753c8adc182be5e0e949104c
fallenhak <135631505+fallenhak@users.noreply.github.com>
2026-09-20 15:31:47 +0300

    refactor: consume normalized party state changes

--- Code/client/Services/AuthorityService.h
+++ Code/client/Services/AuthorityService.h
@@ -1,10 +1,7 @@
 #pragma once
 
 struct World;
-struct PartyJoinedEvent;
-struct PartyLeftEvent;
-struct DisconnectedEvent;
-struct NotifyPartyInfo;
+struct PartyStateChangedEvent;
 
 /**
  * @brief Client-side view of local world authority.
@@ -26,17 +23,11 @@ struct AuthorityService
     [[nodiscard]] uint32_t GetWorldAuthorityPlayerId() const noexcept;
 
 private:
-    void OnPartyJoined(const PartyJoinedEvent& acEvent) noexcept;
-    void OnPartyLeft(const PartyLeftEvent& acEvent) noexcept;
-    void OnPartyInfo(const NotifyPartyInfo& acMessage) noexcept;
-    void OnDisconnected(const DisconnectedEvent& acEvent) noexcept;
+    void OnPartyStateChanged(const PartyStateChangedEvent& acEvent) noexcept;
     void PublishAuthorityChanged() noexcept;
 
     World& m_world;
     entt::dispatcher& m_dispatcher;
 
-    entt::scoped_connection m_partyJoinedConnection;
-    entt::scoped_connection m_partyLeftConnection;
-    entt::scoped_connection m_partyInfoConnection;
-    entt::scoped_connection m_disconnectedConnection;
+    entt::scoped_connection m_partyStateChangedConnection;
 };
